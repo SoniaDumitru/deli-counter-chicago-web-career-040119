8125de20933f67cf327862fc787f9f85f7a3c89c
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-14 19:59:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,17 @@ def line(katz_deli)
       puts "The line is currently empty."
   end
 end
+
 def take_a_number(katz_deli, string)
   katz_deli.push(string)
   puts "Welcome, #{string}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{{line.first}}"
+    katz_deli.shift 
+  end 
+end 
