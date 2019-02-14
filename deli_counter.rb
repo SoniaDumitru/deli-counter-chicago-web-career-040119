246858009b79153f5278b1dc05df4ff2bab1e730
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-14 19:02:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length >= 1
+  new_katz_deli = []
+  i = 1
+    katz_deli.each do |name|
+      new_katz_deli.push("#{i}. #{name}")
+      i+=1
+    end
+    puts "The line is currently: #{katz_deli.join(" ")}"
+  else
+      puts "The line is currently empty."
+  end
+end
