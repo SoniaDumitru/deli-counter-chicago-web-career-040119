c48c14f63ea36d57c07816c9cdef51f32fb2e8db
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-14 19:08:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,12 @@
 katz_deli = []
 def line(katz_deli)
   if katz_deli.length >= 1
-  new_katz_deli = []
-  counter = 1
-    katz_deli.each do |name|
-      new_katz_deli.push("#{counter}. #{name}")
-      counter += 1
-    end
+    new_katz_deli = []
+    counter = 1
+      katz_deli.each do |name|
+        new_katz_deli.push("#{counter}. #{name}")
+        counter += 1
+        end
     puts "The line is currently: #{new_katz_deli.join(" ")}"
   else
       puts "The line is currently empty."
