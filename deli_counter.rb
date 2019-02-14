5b863fdfb5978ec449077873e0fffcfc0c6b1d37
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-14 19:14:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,7 @@ def line(katz_deli)
       puts "The line is currently empty."
   end
 end
+def take_a_number(katz_deli, string)
+  katz_deli.push(string)
+  puts "Welcome, #{string}. You are number " + katz_deli.length +  "in line."
+  
